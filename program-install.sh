a9cba94212e12ca760042b45e695b042dbac91e4
Kevin Kredit <k.kredit.us@ieee.org>
2018-08-28 10:27:00 -0400

    Added Enpass pw manager installation

--- program-install.sh
+++ program-install.sh
@@ -30,3 +30,10 @@ sudo apt-get install -y \
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# Enpass
+echo "deb http://repo.sinew.in/ stable main" | \
+    sudo tee /etc/apt/sources.list.d/enpass.list > /dev/null
+wget -O - https://dl.sinew.in/keys/enpass-linux.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y enpass
