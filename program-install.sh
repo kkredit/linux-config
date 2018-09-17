b271695f838338f3be83c4bd8ba6894601dd41a5
Kevin Kredit <k.kredit.us@ieee.org>
2018-09-17 11:32:14 -0400

    Added 'bat' installation

--- program-install.sh
+++ program-install.sh
@@ -39,6 +39,13 @@ sudo apt-get install -y \
 sudo mv /bin/sh /bin/sh.bak
 sudo ln -s /bin/bash /bin/sh
 
+if has_arg "bat"; then
+    echo "Download latest amd64 package from opened page and exit Firefox"
+    firefox https://github.com/sharkdp/bat/releases
+    sudo dpkg -i ~/Downloads/bat_*_amd64.deb
+    rm ~/Downloads/bat_*_amd64.deb
+fi
+
 if has_arg "docker"; then
     # Docker
     sudo apt-get install -y \
