9aba8bd1d2ec4929104e87416c65d2796dfff00f
Kevin Kredit <k.kredit.us@ieee.org>
2018-11-26 09:24:42 -0500

    Renamed system-files/ -> system_files/

--- file-install.sh
+++ file-install.sh
@@ -2,7 +2,7 @@
 source helper_scripts/has-arg.sh
 
 # Plain files
-FILES_DIR=system-files
+FILES_DIR=system_files
 
 install -m 644 $FILES_DIR/bashrc ~/.bashrc
 install -m 644 $FILES_DIR/bash_aliases ~/.bash_aliases
--- program-install.sh
+++ program-install.sh
@@ -43,12 +43,12 @@ fi
 
 if has_arg "bat"; then
     echo "Download latest amd64 package from opened page and replace the copy"
-    echo "in system-files/"
+    echo "in system_files/"
     firefox https://github.com/sharkdp/bat/releases
 fi
 
 # Bat (https://github.com/sharkdp/bat)
-sudo dpkg -i system-files/bat*.deb
+sudo dpkg -i system_files/bat*.deb
 
 if has_arg "docker"; then
     # Docker
