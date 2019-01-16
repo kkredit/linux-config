d4e7bfd81ce2ddb0d4c7e7c7a961f766ed6c2457
Kevin Kredit <k.kredit.us@ieee.org>
2019-01-16 12:33:55 -0500

    Clean win_prog_install format and user independence

--- wsl/win-program-install.sh
+++ wsl/win-program-install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # WSL only -- use "chocolatey" to install Windows programs
 
-
 # Setup
-WSL_DKTP="/mnt/c/Users/kevinkredit/Desktop/"
-WIN_DKTP="C:\\Users\\kevinkredit\\Desktop"
+WIN_USER=$(powershell.exe '$env:UserName' | sed 's/\r//g')
+WSL_DKTP="/mnt/c/Users/$WIN_USER/Desktop/"
+WIN_DKTP="C:\\Users\\$WIN_USER\\Desktop"
 
 INST_CHOCO=install-choco.bat
 RUN_CHOCO=choco-install-progs.bat
@@ -13,11 +12,9 @@ RUN_CHOCO=choco-install-progs.bat
 unix2dos -n $INST_CHOCO $WSL_DKTP/$INST_CHOCO > /dev/null
 unix2dos -n $RUN_CHOCO $WSL_DKTP/$RUN_CHOCO > /dev/null
 
-
 # Install chocolatey
 powershell.exe -Command \
     "Start-Process cmd -ArgumentList \"/C $WIN_DKTP\\$INST_CHOCO\" -Verb RunAs"
-
 
 # Install my windows programs
 echo "Waiting 15 seconds for install to complete..."
@@ -25,9 +22,6 @@ sleep 15
 powershell.exe -Command \
     "Start-Process cmd -ArgumentList \"/K $WIN_DKTP\\$RUN_CHOCO\" -Verb RunAs"
 
-
-
 # Teardown
-
 echo "Don't forget to delete the Desktop copy of chocolatey-install.bat"
 echo "once the install is complete!"
