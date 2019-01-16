b67ec2dba3e5d47f318d47491f086f74ed056102
Kevin Kredit <k.kredit.us@ieee.org>
2019-01-16 12:34:04 -0500

    Add a number of win install programs

--- wsl/choco-install-progs.bat
+++ wsl/choco-install-progs.bat
@@ -2,11 +2,21 @@
 set PACKAGES= ^
     googlechrome ^
     firefox ^
+    enpass.install ^
+    adobereader ^
+    ^
     7zip.install ^
+    filezilla ^
     windirstat ^
+    autohotkey.portable ^
+    ^
     notepadplusplus ^
     hexedit ^
-    meld
+    meld ^
+    atom ^
+    boostnote ^
+    ^
+    virtualbox
 
 :: Install all the packages
 for %%G in (%PACKAGES%) do choco install %%G -fy
