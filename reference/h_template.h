a4580c8d5ea1f9c8db16b50d649f573886018711
Kevin Kredit <k.kredit.us@ieee.org>
2019-03-28 08:31:55 -0400

    Add .c and .h templates

---
+++ reference/h_template.h
@@ -0,0 +1,55 @@
+/**
+ * \file
+ *
+ * \brief
+ * \details
+ *
+ * \copyright Copyright (c) 2019, Kevin Kredit.
+ */
+
+#ifndef TPL_PUBLIC_H
+#define	TPL_PUBLIC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************
+ *                                                                 Inclusions */
+
+/******************************************************************************
+ *                                                                    Defines */
+
+/******************************************************************************
+ *                                                                      Types */
+
+/******************************************************************************
+ *                                                                Extern data */
+
+/******************************************************************************
+ *                                                      Function declarations */
+
+/** begin section to remove **************************************************/
+#pragma message "Remove template content in " __FILE__ "!"
+/**
+ * \brief
+ * \pre
+ * \post
+ * \param[in]
+ * \param[out]
+ * \return
+ */
+void exampleFunction(void);
+/**
+ * Usage: fill in all appropriate fields; remove unnecessary fields.
+ */
+/***************************************************** end section to remove */
+
+
+/******************************************************************************
+ *                                                                        EOF */
+#ifdef __cplusplus
+}
+#endif
+#endif /* header guard */
+
