4c5f574e342097e3cf00e5d11d3412e06e2bdea2
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-06 16:27:14 -0400

    Install gitconfig by default

--- file-install.sh
+++ file-install.sh
@@ -9,21 +9,17 @@ install -m 644 $FILES_DIR/bash_aliases ~/.bash_aliases
 install -m 644 $FILES_DIR/bash_prompt ~/.bash_prompt
 if [[ -e ~/.bash-git-prompt ]]; then
     rm ~/.bash-git-prompt
-fi;
+fi
 ln -s $(pwd)/submodules/bash-git-prompt ~/.bash-git-prompt
 install -m 644 $FILES_DIR/custom.bgptheme ~/.git-prompt-colors.sh
 install -m 644 $FILES_DIR/vimrc ~/.vimrc
 install -m 644 $FILES_DIR/xinitrc ~/.xinitrc
 install -m 644 $FILES_DIR/Xmodmap ~/.Xmodmap
 install -m 644 $FILES_DIR/tmux.conf ~/.tmux.conf
-
 if [ ! -f ~/.gerritrc ]; then
     install -m 644 $FILES_DIR/gerritrc ~/.gerritrc
 fi
-
-if [ ! -f ~/.gitconfig ]; then
-    install -m 644 $FILES_DIR/gitconfig ~/.gitconfig
-fi
+install -m 644 $FILES_DIR/gitconfig ~/.gitconfig
 install -m 644 $FILES_DIR/gitignore_global ~/.gitignore_global
 
 # Program files
