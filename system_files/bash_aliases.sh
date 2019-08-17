74a20a498f18bd0d933f26b5307eacebc04ee83c
Kevin Kredit <k.kredit.us@ieee.org>
2019-08-16 21:07:41 -0400

    Add some recommended aliases

--- system_files/bash_aliases.sh
+++ system_files/bash_aliases.sh
@@ -5,6 +5,8 @@ alias ls="ls $COLOR_AUTO -F"
 alias ll='ls -alFh'
 alias la='ls -A'
 alias l='ls -CF'
+alias cpv='rsync -ah --info=progress2'
+alias tcn='mv --force -t ~/.local/share/Trash '
 alias grep="grep $COLOR_AUTO"
 alias dos2unixr='find . -type f -exec dos2unix {} \;'
 alias unix2dosr='find . -type f -exec unix2dos {} \;'
