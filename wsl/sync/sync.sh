be0f7c27b08618df58d44e4fb8f4c0e170c78eaa
Kevin Kredit <k.kredit.us@ieee.org>
2019-08-27 17:26:29 -0400

    Simplify wsl sync script

--- wsl/sync/sync.sh
+++ wsl/sync/sync.sh
@@ -1,29 +1,25 @@
 
 # see 'man rsync' or https://linux.die.net/man/1/rsync
 
-DEST_USER=Kevin
+# Config
+DEST_USER=kevin
 DEST_IP=192.168.0.194
-DEST_USR_DIR=/cygdrive/c/Users/Kevin
-HOST_USR_DIR=/cygdrive/c/Users/kevin
-DIRS=(Music Documents Pictures Videos)
-SEND_OPTIONS=rtvuz
-RECV_OPTIONS=rtvuz
+SYNC_DIR=/mnt/c/sync/
+
 # options:
 #   C: auto ignore files in CVS manner
-#   a: archive (-rlptgoD) -- this breaks Windows permissions! At least on the filesystem on the remote machine
+#   a: archive (-rlptgoD) -- this breaks Windows permissions!
 #   r: recursive
 #   t: preserve times
 #   v: verbose
 #   u: update (skip newer files on receiver)
 #   z: compress
+SEND_OPTIONS=rtvuz
+RECV_OPTIONS=rtvuz
 
-for DIR in ${DIRS[@]}; do
-    # push out local changes -- but keep newer files there
-    rsync -$SEND_OPTIONS --exclude-from='exclude_list.txt' \
-        $HOST_USR_DIR/$DIR/ \
-        $DEST_USER@$DEST_IP:$DEST_USR_DIR/$DIR/
-    # pull in remote changes -- but keep newer files here
-    rsync -$RECV_OPTIONS --exclude-from='exclude_list.txt' \
-        $DEST_USER@$DEST_IP:$DEST_USR_DIR/$DIR/ \
-        $HOST_USR_DIR/$DIR/
-done
+# push out local changes -- but keep newer files there
+rsync -$SEND_OPTIONS --exclude-from='exclude_list.txt' \
+    $SYNC_DIR $DEST_USER@$DEST_IP:$SYNC_DIR
+# pull in remote changes -- but keep newer files here
+rsync -$RECV_OPTIONS --exclude-from='exclude_list.txt' \
+    $DEST_USER@$DEST_IP:$SYNC_DIR $SYNC_DIR
