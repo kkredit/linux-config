1069efa679745cf88649b3a09aa34a3925f80550
Kevin Kredit <k.kredit.us@ieee.org>
2019-10-04 14:09:58 -0400

    Add firacode linux install

--- program-install.sh
+++ program-install.sh
@@ -47,6 +47,30 @@ if has_arg "vscodium"; then
     cat $FILES_DIR/VSCodium/extensions.txt | xargs -n 1 codium --install-extension
 fi
 
+if has_arg "firacode"; then
+    fonts_dir="${HOME}/.local/share/fonts"
+    if [ ! -d "${fonts_dir}" ]; then
+        echo "mkdir -p $fonts_dir"
+        mkdir -p "${fonts_dir}"
+    else
+        echo "Found fonts dir $fonts_dir"
+    fi
+
+    for type in Bold Light Medium Regular Retina; do
+        file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+        file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+        if [ ! -e "${file_path}" ]; then
+            echo "wget -O $file_path $file_url"
+            wget -O "${file_path}" "${file_url}"
+        else
+            echo "Found existing file $file_path"
+        fi;
+    done
+
+    echo "fc-cache -f"
+    fc-cache -f
+fi
+
 if has_arg "git"; then
     # This adds a new remote repo that hosts more up-to-date versions of git
     sudo add-apt-repository ppa:git-core/ppa
