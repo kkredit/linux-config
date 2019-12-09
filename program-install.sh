19f88496c9ee07b18c7bc8df98a147cdf979b70d
Kevin Kredit <k.kredit.us@ieee.org>
2019-12-09 14:11:13 -0500

    Add Chrome install

--- program-install.sh
+++ program-install.sh
@@ -330,3 +330,11 @@ if has_arg "sonarqube"; then
     rm $FILE
 fi
 
+if has_arg "chrome"; then
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | \
+        sudo tee /etc/apt/sources.list.d/google-chrome.list
+    sudo-pkg-mgr update
+    sudo-pkg-mgr install google-chrome-stable
+fi
+
