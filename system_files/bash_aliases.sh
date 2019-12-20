15987ea9eb94c30dc54334344832148db8f08d61
Kevin Kredit <k.kredit.us@ieee.org>
2019-12-20 14:48:47 -0500

    Add alias to strip_term_codes
    See https://stackoverflow.com/questions/17998978/removing-colors-from-output#18000433

--- system_files/bash_aliases.sh
+++ system_files/bash_aliases.sh
@@ -41,4 +41,5 @@ alias ddg='graphene ddg'
 alias explorer='nautilus .'
 alias txt2env='mkenvimage'
 alias dsf='git diff --no-index --color'
+alias strip_term_codes="sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'"
 
