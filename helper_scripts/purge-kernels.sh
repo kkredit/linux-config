66019acc74da7d0b99e5fdbad4f1d66e63f269f0
Kevin Kredit <k.kredit.us@ieee.org>
2020-03-12 17:39:47 -0400

    Add purge-kernels helper script

---
+++ helper_scripts/purge-kernels.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+# Thanks to Stewart Hildebrand for this script! https://github.com/stewdk, http://www.stew.dk/
+# Copyright 2020 Stew Hildebrand
+# SPDX-License-Identifier: MIT
+
+KERNEL_VARIANT="$(uname -r | awk -F "-" '{print $NF}')"
+KERNEL_VERSION="$(uname -r | sed -e "s/-${KERNEL_VARIANT}//")"
+
+echo "Currently running: ${KERNEL_VERSION}-${KERNEL_VARIANT}"
+
+ALL_VER="$(dpkg --list | grep -e linux-image -e linux-headers | grep -v -e linux-headers-${KERNEL_VARIANT} -e linux-image-${KERNEL_VARIANT} | awk '{print $3}' | sed 's/+.*$//' | sort -uV)"
+
+echo "All installed:" ${ALL_VER}
+
+NUM_INSTALLED="$(echo "${ALL_VER}" | wc -l)"
+
+if [ ${NUM_INSTALLED} -lt 3 ]; then
+    echo "2 or fewer kernel versions installed, quitting"
+    exit
+fi
+
+NUM_TO_REMOVE="$(expr ${NUM_INSTALLED} - 2)"
+REMOVE_VER="$(echo "${ALL_VER}" | head -n ${NUM_TO_REMOVE})"
+REMOVE_PACKAGES="$(dpkg --list | grep -e "${REMOVE_VER}" | grep -v libc | awk '{print $2}')"
+
+echo "Removing:" ${REMOVE_PACKAGES}
+
+sudo apt-get purge ${REMOVE_PACKAGES}
