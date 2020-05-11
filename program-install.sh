22bdd92f4b0c63d93af8f8ba5dfd7d6570382afb
Kevin Kredit <k.kredit.us@ieee.org>
2020-05-11 18:03:23 -0400

    Add aws-cli program install

--- program-install.sh
+++ program-install.sh
@@ -464,3 +464,12 @@ if has_arg "glow"; then
     sudo dpkg -i $FILE
     rm $FILE
 fi
+
+if has_arg "awscli"; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    aws --version
+    rm -r awscliv2.zip aws
+fi
+
