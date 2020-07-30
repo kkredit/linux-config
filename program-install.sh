e969811f97e73ec0791073fc0cdb1e4c3c4c1c31
Kevin Kredit <k.kredit.us@ieee.org>
2020-07-30 10:52:00 -0400

    Add docker-compose install

--- program-install.sh
+++ program-install.sh
@@ -391,6 +391,11 @@ if has_arg "docker"; then
     # Docker installation process should create group 'docker'
     # sudo groupadd docker
     sudo usermod -aG docker $USER
+
+    # Docker Compose
+    URL="https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)"
+    sudo curl -L "$URL" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 if has_arg "wireshark"; then
