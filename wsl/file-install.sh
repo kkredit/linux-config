832b67a9170d8630ba0e1adc9791bf434ba9a66b
Kevin Kredit <k.kredit.us@ieee.org>
2020-08-01 10:12:29 -0400

    Add 'Move Mouse' settings
    Intended to automate startup and restart of Tether on
    an unmonitored computer.

--- wsl/file-install.sh
+++ wsl/file-install.sh
@@ -10,5 +10,8 @@ cp images/* /mnt/c/wsl/icons/
 cp windows-terminal-settings.json settings.json
 sed -i "s/WIN_USER/$WIN_USER/g" settings.json
 WIN_TERM_SETTINGS=$(ls /mnt/c/Users/$WIN_USER/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json)
-unix2dos -n settings.json $WIN_TERM_SETTINGS
+unix2dos -n settings.json "$WIN_TERM_SETTINGS"
 rm settings.json
+
+MOVE_MOUSE_SETTINGS="/mnt/c/Users/Kevin/AppData/Local/Packages/1258EllAbi.MoveMouse_hjfwaxvfbwh7t/LocalCache/Roaming/Ellanet/Move Mouse/Settings.xml"
+unix2dos -n move-mouse-settings.xml "$MOVE_MOUSE_SETTINGS"
