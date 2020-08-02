9ed7e8c2f137ecb331ec5b4ed5c18b0b4ce9eac4
Kevin Kredit <k.kredit.us@ieee.org>
2020-08-02 11:08:05 -0400

    Update Move Mouse settings, but disable

--- wsl/file-install.sh
+++ wsl/file-install.sh
@@ -13,5 +13,5 @@ WIN_TERM_SETTINGS=$(ls /mnt/c/Users/$WIN_USER/AppData/Local/Packages/Microsoft.W
 unix2dos -n settings.json "$WIN_TERM_SETTINGS"
 rm settings.json
 
-MOVE_MOUSE_SETTINGS="/mnt/c/Users/Kevin/AppData/Local/Packages/1258EllAbi.MoveMouse_hjfwaxvfbwh7t/LocalCache/Roaming/Ellanet/Move Mouse/Settings.xml"
-unix2dos -n move-mouse-settings.xml "$MOVE_MOUSE_SETTINGS"
+# MOVE_MOUSE_SETTINGS="/mnt/c/Users/Kevin/AppData/Local/Packages/1258EllAbi.MoveMouse_hjfwaxvfbwh7t/LocalCache/Roaming/Ellanet/Move Mouse/Settings.xml"
+# unix2dos -n move-mouse-settings.xml "$MOVE_MOUSE_SETTINGS"
