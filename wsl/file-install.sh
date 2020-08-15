dcf3770d3187b049afb65a929a68f188013ff2fc
Kevin Kredit <k.kredit.us@ieee.org>
2020-08-15 10:27:51 -0400

    Cleanup WSL file-install

--- wsl/file-install.sh
+++ wsl/file-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ../helper_scripts/local-helpers.sh
-
 $WSL || exit 1
 
 mkdir -p /mnt/c/wsl/icons
@@ -10,6 +8,6 @@ cp images/* /mnt/c/wsl/icons/
 cp windows-terminal-settings.json settings.json
 sed -i "s/WIN_USER/$WIN_USER/g" settings.json
 WIN_TERM_SETTINGS=$(ls /mnt/c/Users/$WIN_USER/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json)
-unix2dos -n settings.json "$WIN_TERM_SETTINGS"
+unix2dos -n settings.json "$WIN_TERM_SETTINGS" 2>/dev/null
 rm settings.json
 
