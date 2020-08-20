7c58a994aa19cc755ec41b2a6313ad444f602447
Kevin Kredit <k.kredit.us@ieee.org>
2020-08-20 10:16:37 -0400

    Add ability to update AWS CLI

--- program-install.sh
+++ program-install.sh
@@ -485,7 +485,7 @@ fi
 if has_arg "awscli"; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
-    sudo ./aws/install
+    which aws &>/dev/null && sudo ./aws/install --update || sudo ./aws/install
     aws --version
     rm -r awscliv2.zip aws
 fi
