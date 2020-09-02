dbd01fe92b067c2094869a5c435bfeb56423eb68
Kevin Kredit <k.kredit.us@ieee.org>
2020-09-02 09:24:24 -0400

    Bump submodules and add Windows Terminal context menu entry

--- wsl/file-install.sh
+++ wsl/file-install.sh
@@ -11,3 +11,4 @@ WIN_TERM_SETTINGS=$(ls /mnt/c/Users/$WIN_USER/AppData/Local/Packages/Microsoft.W
 unix2dos -n settings.json "$WIN_TERM_SETTINGS" 2>/dev/null
 rm settings.json
 
+run_ps1 $(git top)/wsl/ps_scripts/setup-wt-context-menu.ps1
---
+++ wsl/ps_scripts/setup-wt-context-menu.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+cd $PSScriptRoot
+
+./../../submodules/wt-context-menu/SetupContextMenu.ps1
+
