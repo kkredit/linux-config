b1daf8fcf25a25f92ce507d31a8a26a6c661d183
Kevin Kredit <k.kredit.us@ieee.org>
2021-04-14 16:44:28 -0400

    Add some helpful functions to the bash template

--- reference/bash_template.sh
+++ reference/bash_template.sh
@@ -41,5 +41,15 @@ done
 ################################################################################
 #                                                                       SCRIPT #
 
+function header_print {
+  printf "\n===============================================================================\n"
+  echo "$@"
+  printf "===============================================================================\n\n"
+}
+
+function run_on_target {
+  ssh user@ip_addr bash -esx <<<"$1"
+}
+
 # shellcheck disable=SC2086
 # Use ^^ to disable a specific shellcheck linting rule
