0f4abfd1971f1beb9fc7e206cb6be7a4970d3518
Kevin Kredit <k.kredit.us@ieee.org>
2022-01-21 16:13:34 -0500

    Remove and update submodules

--- file-install.sh
+++ file-install.sh
@@ -45,69 +45,21 @@ if has_arg "dconf"; then
 fi
 
 # Submodules files
+set -x
 if has_arg "submodules" || [[ 0 == $(find submodules/ -type f | wc -l) ]]; then
     git submodule init
     git submodule update --init --force --remote
+
+    pushd submodules/autojump > /dev/null || true
+    ./install.py > /dev/null
+    popd > /dev/null || true
 fi
 
 mkdir -p ~/bin
+
 printf 'y\ny\nn\n' | ./submodules/fzf/install &> /dev/null
 install -m 755 submodules/diff-so-fancy/diff-so-fancy ~/bin/diff-so-fancy
 cp -r submodules/diff-so-fancy/lib ~/bin/
 install -m 755 submodules/git-log-compact/git-log-compact \
     ~/bin/git-log-compact
-install -m 755 submodules/tldr/tldr ~/bin/tldr
-if $DO_UPDATE; then
-    pushd submodules/autojump > /dev/null || true
-    ./install.py > /dev/null
-    popd > /dev/null || true
-fi
-install -m 644 submodules/rails_completion/rails.bash ~/.rails.bash
-install -m 644 submodules/forgit/forgit.plugin.sh ~/.forgit.plugin.sh
-install -m 755 submodules/git-heatmap/git-heatmap ~/bin/
-
-# VSCodium
-if has_arg "codium" && [[ $(which codium) ]]; then
-    if $MAC; then
-        VSC_CONF_DIR=~/Library/Application\ Support/VSCodium/User
-    else
-        VSC_CONF_DIR=~/.config/VSCodium/User
-    fi
-    RUN_VSC=codium
-    function INST_FILE() { install -m 644 "$@"; }
-    function GET_FILE() { install -m 644 "$@"; }
-    if $WSL; then
-        VSC_CONF_DIR="/mnt/c/Users/$WIN_USER/AppData/Roaming/VSCodium/User"
-        RUN_VSC='run_cmd codium'
-        function INST_FILE() { unix2dos -n "$1" "$2" 2>/dev/null; }
-        function GET_FILE() { dos2unix -n "$1" "$2" 2>/dev/null; chmod 644 "$2"; }
-    fi
-    mkdir -p "$VSC_CONF_DIR"
-    for FILE in keybindings.json settings.json; do
-        if [[ ! -f "$VSC_CONF_DIR"/$FILE ]]; then
-            INST_FILE $FILES_DIR/VSCodium/$FILE "$VSC_CONF_DIR"/$FILE
-        elif [[ "" != "$(diff --strip-trailing-cr "$VSC_CONF_DIR"/$FILE \
-                                                  $FILES_DIR/VSCodium/$FILE)" ]]
-        then
-            if [[ $(stat -c %Y "$VSC_CONF_DIR"/$FILE) < \
-                  $(stat -c %Y system_files/VSCodium/$FILE) ]]
-            then
-                INST_FILE $FILES_DIR/VSCodium/$FILE "$VSC_CONF_DIR"/$FILE
-            else
-                GET_FILE "$VSC_CONF_DIR"/$FILE $FILES_DIR/VSCodium/$FILE
-                echo "Local VSCodium $FILE settings newer than tracked. Settings copied here."
-            fi
-        fi
-    done
-    if ! $WSL; then
-        EXTENSIONS=$($RUN_VSC --list-extensions | grep -v simple-vim | sed 's/\r//g')
-        if [[ "$EXTENSIONS" != "$(cat $FILES_DIR/VSCodium/extensions.txt)" ]]
-        then
-            echo "$EXTENSIONS" > $FILES_DIR/VSCodium/extensions.txt
-            echo "Local VSCodium extensions different than tracked. List updated here."
-            echo "Determine desired list of extensions and run"
-            echo "    cat system_files/VSCodium/extensions.txt | "
-            echo "        xargs -n 1 -I {} bash -c \"$RUN_VSC --install-extension \\\$1\" _ {}"
-        fi
-    fi
-fi
+install -m 755 submodules/tldr-sh-client/tldr ~/bin/tldr
