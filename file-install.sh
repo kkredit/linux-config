9250b06d62321b4811dead7c637070a38dd3d90a
Kevin Kredit <k.kredit.us@ieee.org>
2022-02-13 11:53:42 -0500

    Add alacritty installation and config

--- file-install.sh
+++ file-install.sh
@@ -21,6 +21,8 @@ install -m 644 $FILES_DIR/dircolors.sh ~/.dircolors
 install -m 644 $FILES_DIR/vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
 install -m 644 $FILES_DIR/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/alacritty
+install -m 644 $FILES_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml
 install -m 644 $FILES_DIR/coc-settings.nvim.json ~/.config/nvim/coc-settings.json
 install -m 644 $FILES_DIR/xinitrc ~/.xinitrc
 install -m 644 $FILES_DIR/Xmodmap ~/.Xmodmap
@@ -33,10 +35,9 @@ install -m 644 $FILES_DIR/ripgreprc ~/.ripgreprc
 
 # WSL files
 if $WSL; then
-    install -m 644 $WSL_FILES_DIR/bashrc_wsl.sh ~/.bashrc_wsl
-    unix2dos -n $FILES_DIR/gitconfig_windows "$(wslpath 'C:/ProgramData/Git/config')" 2>/dev/null
-    unix2dos -n $FILES_DIR/gitignore_global "$(wslpath 'C:/ProgramData/Git/gitignore_global')" \
-        2>/dev/null
+  install -m 644 $WSL_FILES_DIR/bashrc_wsl.sh ~/.bashrc_wsl
+  unix2dos -n $FILES_DIR/gitconfig_windows "$(wslpath 'C:/ProgramData/Git/config')" 2>/dev/null
+  unix2dos -n $FILES_DIR/gitignore_global "$(wslpath 'C:/ProgramData/Git/gitignore_global')" 2>/dev/null
 fi
 
 if has_arg "dconf"; then
@@ -46,12 +47,12 @@ fi
 
 # Submodules files
 if has_arg "submodules" || [[ 0 == $(find submodules/ -type f | wc -l) ]]; then
-    git submodule init
-    git submodule update --init --force --remote
+  git submodule init
+  git submodule update --init --force --remote
 
-    pushd submodules/autojump > /dev/null || true
-    ./install.py > /dev/null
-    popd > /dev/null || true
+  pushd submodules/autojump > /dev/null || true
+  ./install.py > /dev/null
+  popd > /dev/null || true
 fi
 
 mkdir -p ~/bin
@@ -62,3 +63,21 @@ cp -r submodules/diff-so-fancy/lib ~/bin/
 install -m 755 submodules/git-log-compact/git-log-compact \
     ~/bin/git-log-compact
 install -m 755 submodules/tldr-sh-client/tldr ~/bin/tldr
+
+# Install Alacritty
+if has_arg "alacritty"; then
+  if ! which cargo &>/dev/null; then
+    echo "!! Must install Rust to install Alacritty (with ligatures)"
+    exit 1
+  fi
+
+  pushd submodules/alacritty-ligature > /dev/null || true
+  if $MAC; then
+    make app
+    cp -r target/release/osx/Alacritty.app /Applications/
+  else
+    apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+    cargo build --release
+  fi
+  popd > /dev/null || true
+fi
