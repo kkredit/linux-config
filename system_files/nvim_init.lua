533b80879ec0a6747e491303b0758f8e293407aa
Kevin Kredit <knkredit60@gmail.com>
2023-01-30 19:56:20 -0500

    Nvim: fix <leader>d lag

--- system_files/nvim_init.lua
+++ system_files/nvim_init.lua
@@ -408,8 +408,8 @@ require 'nvim-treesitter.configs'.setup {
       border = 'none',
       floating_preview_opts = {},
       peek_definition_code = {
-        ["<leader>df"] = "@function.outer",
-        ["<leader>dF"] = "@class.outer",
+        ["<leader>pf"] = "@function.outer",
+        ["<leader>pF"] = "@class.outer",
       },
     },
   },
