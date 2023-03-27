b36859c18503f1a44e0a8b4145557c5c559c5bf9
Kevin Kredit <knkredit60@gmail.com>
2023-03-27 15:08:17 -0400

    Nvim: add copilot, reduce snippets

--- system_files/nvim_init.lua
+++ system_files/nvim_init.lua
@@ -5,15 +5,15 @@ vim.cmd('source ~/.vimrc')
 local cmp = require('cmp')
 
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
+  -- snippet = {
+    -- -- REQUIRED - you must specify a snippet engine
+    -- expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    -- end,
+  -- },
 
   window = {
     completion = cmp.config.window.bordered(),
@@ -30,7 +30,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
     { name = 'path' },
-    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
