43c8fd2d6eb86cba62b131a3f96372ab6e9df666
Kevin Kredit <knkredit60@gmail.com>
2025-11-21 09:43:18 -0500

    Update leap config

--- system_files/nvim_init.lua
+++ system_files/nvim_init.lua
@@ -577,11 +577,25 @@ telescope.load_extension('fzf')
 vim.cmd("set foldmethod=expr")
 vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
 
+-- Leap
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+require('leap').opts.preview = function (ch0, ch1, ch2)
+  return not (
+    ch1:match('%s')
+    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+  )
+end
+require('leap').opts.equivalence_classes = {
+  ' \t\r\n', '([{', ')]}', '\'"`'
+}
+require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+-- Other plugins
 require("trouble").setup {
   focus = true,
   auto_preview = false,
 }
-require('leap').set_default_keymaps()
 require('gitsigns').setup()
 require('which-key').setup()
 ---@diagnostic disable-next-line: missing-fields
